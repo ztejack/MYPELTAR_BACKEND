46ae90c1f1ad9ba48e3e2e45a9a25b0924d39e7d
ztejack <axtomy9@gmail.com>
2024-07-11 12:11:10 +0700

    update Route

--- routes/api.php
+++ routes/api.php
@@ -55,12 +55,12 @@ Route::prefix('v1/client')->middleware(['admin'])->group(
 // );
 Route::prefix('v1/user')->middleware(['auth:api', 'api.key'])->group(
     function () {
-        Route::get('getall', [UserController::class, 'index'])->middleware('can:getall-users');
-        Route::get('search', [UserController::class, 'search'])->middleware('can:search-users');
-        Route::post('store', [UserController::class, 'store'])->middleware('can:store-users');
-        Route::get('show/{user}', [UserController::class, 'show'])->middleware('can:show-users');
-        Route::patch('update/{user}', [UserController::class, 'update'])->middleware('can:update-users');
-        Route::post('destroy/{user}', [UserController::class, 'destroy'])->middleware('can:delete-users');
+        Route::get('getall', [UserController::class, 'index']);
+        Route::get('search', [UserController::class, 'search']);
+        Route::post('store', [UserController::class, 'store']);
+        Route::get('show/{user}', [UserController::class, 'show']);
+        Route::patch('update/{user}', [UserController::class, 'update']);
+        Route::post('destroy/{user}', [UserController::class, 'destroy']);
     }
 );
 
@@ -68,10 +68,10 @@ Route::prefix('v1/asset')->middleware('auth:api')->group(
     function () {
         Route::get('getall', [AssetController::class, 'index']);
         Route::get('search', [AssetController::class, 'search']);
-        Route::post('store', [AssetController::class, 'store'])->middleware(['can:store-assets']);
+        Route::post('store', [AssetController::class, 'store']);
         Route::get('show/{asset}', [AssetController::class, 'show']);
-        Route::post('update/{asset}', [AssetController::class, 'update'])->middleware(['can:update-assets']);
-        Route::post('destroy/{asset}', [AssetController::class, 'destroy'])->middleware(['can:delete-assets']);
+        Route::post('update/{asset}', [AssetController::class, 'update']);
+        Route::post('destroy/{asset}', [AssetController::class, 'destroy']);
     }
 );
 
@@ -80,17 +80,17 @@ Route::prefix('v1/maintenance')->middleware('auth:api')->group(
         Route::get('getall', [MaintenanceController::class, 'index']);
         Route::get('maintenance_aplly/{maintenance}', [MaintenanceController::class, 'maintenance_aplly']);
         Route::get('self_get', [MaintenanceController::class, 'self_get']);
-        // Route::post('store', [MaintenanceController::class, 'store'])->middleware(['can:store-maintenance']);
+        // Route::post('store', [MaintenanceController::class, 'store']);
         Route::get('show/{maintenance}', [MaintenanceController::class, 'show']);
-        // Route::post('update/{maintenance}', [MaintenanceController::class, 'update'])->middleware(['can:update-maintenance']);
-        Route::post('destroy/{maintenance}', [MaintenanceController::class, 'destroy'])->middleware(['can:delete-maintenance']);
+        // Route::post('update/{maintenance}', [MaintenanceController::class, 'update']);
+        Route::post('destroy/{maintenance}', [MaintenanceController::class, 'destroy']);
 
         Route::prefix("/{maintenance}/history")->middleware('auth:api')->group(
             function () {
-                Route::post('update/{pupdate}', [PUpdateController::class, 'update'])->middleware(['can:update-maintenance']);
-                Route::post('show/{pupdate}', [PUpdateController::class, 'update'])->middleware(['can:update-maintenance']);
-                Route::post('store', [PUpdateController::class, 'update'])->middleware(['can:update-maintenance']);
-                Route::post('destroy/{pupdate}', [PUpdateController::class, 'update'])->middleware(['can:update-maintenance']);
+                Route::post('update/{pupdate}', [PUpdateController::class, 'update']);
+                Route::post('show/{pupdate}', [PUpdateController::class, 'update']);
+                Route::post('store', [PUpdateController::class, 'update']);
+                Route::post('destroy/{pupdate}', [PUpdateController::class, 'update']);
             }
         );
     }
@@ -98,29 +98,29 @@ Route::prefix('v1/maintenance')->middleware('auth:api')->group(
 
 Route::prefix('v1/inspeksi')->middleware('auth:api')->group(
     function () {
-        Route::post('store', [MaintenanceController::class, 'store'])->middleware(['can:store-inspeksi']);
-        Route::post('update/{maintenance}', [MaintenanceController::class, 'update'])->middleware(['can:update-inspeksi']);
+        Route::post('store', [MaintenanceController::class, 'store']);
+        Route::post('update/{maintenance}', [MaintenanceController::class, 'update']);
     }
 );
 
 Route::prefix('v1/category')->middleware('auth:api')->group(
     function () {
         Route::get('getall', [CategoryController::class, 'index']);
-        Route::post('store', [CategoryController::class, 'store'])->middleware(['can:store-category']);
+        Route::post('store', [CategoryController::class, 'store']);
         Route::get('show/{category}', [CategoryController::class, 'show']);
         Route::get('search', [CategoryController::class, 'search']);
-        Route::post('update/{category}', [CategoryController::class, 'update'])->middleware(['can:update-category']);
-        Route::post('destroy/{category}', [CategoryController::class, 'destroy'])->middleware(['can:delete-category']);
+        Route::post('update/{category}', [CategoryController::class, 'update']);
+        Route::post('destroy/{category}', [CategoryController::class, 'destroy']);
     }
 );
 
 Route::prefix('v1/location')->middleware('auth:api')->group(
     function () {
         Route::get('getall', [LocationController::class, 'index']);
-        Route::post('store', [LocationController::class, 'store'])->middleware(['can:store-location']);
+        Route::post('store', [LocationController::class, 'store']);
         Route::get('show/{location}', [LocationController::class, 'show']);
-        Route::post('update/{location}', [LocationController::class, 'update'])->middleware(['can:update-location']);
-        Route::post('destroy/{location}', [LocationController::class, 'destroy'])->middleware(['can:delete-location']);
+        Route::post('update/{location}', [LocationController::class, 'update']);
+        Route::post('destroy/{location}', [LocationController::class, 'destroy']);
         Route::get('search', [LocationController::class, 'search']);
     }
 );
@@ -128,20 +128,20 @@ Route::prefix('v1/location')->middleware('auth:api')->group(
 Route::prefix('v1/satker')->middleware('auth:api')->group(
     function () {
         Route::get('getall', [SatkerController::class, 'index']);
-        Route::post('store', [SatkerController::class, 'store'])->middleware(['can:store-satker']);
+        Route::post('store', [SatkerController::class, 'store']);
         Route::get('show/{satker}', [SatkerController::class, 'show']);
-        Route::post('update/{satker}', [SatkerController::class, 'update'])->middleware(['can:update-satker']);
-        Route::post('destroy/{satker}', [SatkerController::class, 'destroy'])->middleware(['can:delete-satker']);
+        Route::post('update/{satker}', [SatkerController::class, 'update']);
+        Route::post('destroy/{satker}', [SatkerController::class, 'destroy']);
     }
 );
 
 Route::prefix('v1/subsatker')->middleware('auth:api')->group(
     function () {
         Route::get('getall', [SubsatkerController::class, 'index']);
-        Route::post('store', [SubsatkerController::class, 'store'])->middleware(['can:store-subsatker']);
+        Route::post('store', [SubsatkerController::class, 'store']);
         Route::get('show/{subsatker}', [SubsatkerController::class, 'show']);
-        Route::post('update/{subsatker}', [SubsatkerController::class, 'update'])->middleware(['can:update-subsatker']);
-        Route::post('destroy/{subsatker}', [SubsatkerController::class, 'destroy'])->middleware(['can:delete-subsatker']);
+        Route::post('update/{subsatker}', [SubsatkerController::class, 'update']);
+        Route::post('destroy/{subsatker}', [SubsatkerController::class, 'destroy']);
     }
 );
 
@@ -157,10 +157,10 @@ Route::prefix('v1/role')->middleware(['auth:api', 'can:roleManagement'])->group(
 Route::prefix('v1/statusa')->middleware('auth:api')->group(
     function () {
         Route::get('getall', [StatusController::class, 'index']);
-        Route::post('store', [StatusController::class, 'store'])->middleware(['can:store-statusa']);
+        Route::post('store', [StatusController::class, 'store']);
         Route::get('show/{statusassets}', [StatusController::class, 'show']);
-        Route::post('update/{statusassets}', [StatusController::class, 'update'])->middleware(['can:update-statusa']);
-        Route::post('destroy/{statusassets}', [StatusController::class, 'destroy'])->middleware(['can:delete-statusa']);
+        Route::post('update/{statusassets}', [StatusController::class, 'update']);
+        Route::post('destroy/{statusassets}', [StatusController::class, 'destroy']);
     }
 );
 
@@ -169,7 +169,7 @@ Route::prefix('v1/news')->middleware('auth:api')->group(
         Route::get('getall', [NewsController::class, 'index']);
         Route::post('store', [NewsController::class, 'store']);
         Route::get('show/{news}', [NewsController::class, 'show']);
-        Route::post('update/{news}', [NewsController::class, 'update'])->middleware(['can:update-statusa']);
-        Route::post('destroy/{news}', [NewsController::class, 'destroy'])->middleware(['can:delete-statusa']);
+        Route::post('update/{news}', [NewsController::class, 'update']);
+        Route::post('destroy/{news}', [NewsController::class, 'destroy']);
     }
 );
