6f6c13bf3b5ce5c583f8f073d046e41b7fa7d1d1
ztejack <axtomy9@gmail.com>
2025-01-13 02:58:08 +0700

    update gitignore

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -91,13 +91,13 @@
   ),
   'spatie/laravel-ignition' => 
   array (
-    'providers' => 
-    array (
-      0 => 'Spatie\\LaravelIgnition\\IgnitionServiceProvider',
-    ),
     'aliases' => 
     array (
       'Flare' => 'Spatie\\LaravelIgnition\\Facades\\Flare',
+    ),
+    'providers' => 
+    array (
+      0 => 'Spatie\\LaravelIgnition\\IgnitionServiceProvider',
     ),
   ),
   'spatie/laravel-permission' => 
